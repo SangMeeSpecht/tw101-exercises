99d14ca2cb039f9ecc2633f27ccbf8f17ea970c9
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-08 11:53:35 -0600

    Add isOdd method and print statements

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -3,7 +3,20 @@ package com.thoughtworks.tw101.exercises.exercise1;
 // Exercise #1: Print all odd numbers from 1 to 100 and their sum.
 
 public class Main {
-    public static void main(String[] args) {
 
+    private static boolean isOdd(int number) {
+        return (number % 2 != 0);
+    }
+
+    public static void main(String[] args) {
+        int currentSum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (isOdd(i)) {
+                currentSum += i;
+                System.out.println(i);
+                System.out.println("sum: " + currentSum);
+            }
+        }
     }
 }
