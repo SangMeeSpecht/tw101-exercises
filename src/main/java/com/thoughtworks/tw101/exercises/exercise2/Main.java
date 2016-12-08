6d64fb4e727c18e7d5c122f1cc9c935bc550a657
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-08 13:11:41 -0600

    Add #totalMethodCalls and #main

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Main.java
@@ -4,9 +4,20 @@ package com.thoughtworks.tw101.exercises.exercise2;
 // when you call total(). Create a main method that calls increment() 5 times and then calls total().
 
 public class Main {
+
+    public static void totalMethodCalls(int numOfMethodCalls, Accumulator acc) {
+        for (int i = 0; i < numOfMethodCalls; i++) {
+            acc.increment();
+        }
+
+        acc.total();
+    }
+
     public static void main(String[] args) {
 
         Accumulator accumulator = new Accumulator();
+
+        totalMethodCalls(5, accumulator);
 
 
     }
