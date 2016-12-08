24e013a9bc32ad83a83167c6030db099793c60fe
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-08 14:26:31 -0600

    Add #isOdd and #of

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -2,7 +2,16 @@ package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
 
-    public int of(int start, int end) {
-        return -1;
+    private int currentSum = 0;
+
+    private boolean isOdd(int number) { return (number % 2 != 0); }
+
+    public Integer of(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                currentSum += i;
+            }
+        }
+        return currentSum;
     }
 }
