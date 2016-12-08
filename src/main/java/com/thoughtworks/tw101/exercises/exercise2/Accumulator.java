709f63a698c414c80b45b8ced2c391d8bf0230d3
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-08 13:09:54 -0600

    Add increment and total methods

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    int methodCallCounter = 0;
+
+    public void increment() {
+        methodCallCounter += 1;
+    }
+
+    public void total() {
+        System.out.println(methodCallCounter);
+    }
 }
