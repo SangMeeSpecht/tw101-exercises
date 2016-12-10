cfe8275e58e81cdc97fa596de187c2337b689999
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-10 15:06:49 -0600

    Add #chooseNumberMessage

---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/View.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.tw101.exercises.exercise7;
+
+
+public class View {
+    public void chooseNumberMessage(int startNumber, int endNumber) {
+        System.out.println("Please choose a number between " + startNumber + " and " + endNumber);
+    }
+}
