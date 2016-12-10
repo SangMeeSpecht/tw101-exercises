ba6a5cea46eeb9fadd5ef74b84452b7b13aebadf
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-10 11:02:30 -0600

    Add instance variables and fill out #takeDamage and #reportStatus

--- src/main/java/com/thoughtworks/tw101/exercises/exercise6/Troll.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise6/Troll.java
@@ -3,15 +3,21 @@ package com.thoughtworks.tw101.exercises.exercise6;
 
 public class Troll implements Monster {
 
-    private int damageTaken = 0;
+    public String name;
+    private int hp = 0;
+
+    public Troll(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
 
     @Override
     public void takeDamage(int amount) {
-
+        hp -= amount / 2;
     }
 
     @Override
     public void reportStatus() {
-
+        System.out.println(name + " has " + hp + "hp.");
     }
 }
