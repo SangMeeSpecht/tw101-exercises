6eec03a0b2b6e7935b99cc45d38feebcfce13e34
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-13 19:50:11 -0600

    Add to #add

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -3,13 +3,30 @@ package com.thoughtworks.tw101.exercises.exercise9;
 import java.util.List;
 
 public class Node {
-    private String name;
+    public String name;
+    public Node left;
+    public Node right;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        if (nameOfNewNode.compareTo(name) > 0) {
+            if (right == null) {
+                Node node = new Node(nameOfNewNode);
+                right = node;
+            } else {
+                right.add(nameOfNewNode);
+            }
+        } else {
+            if (left == null) {
+                Node node = new Node(nameOfNewNode);
+                left = node;
+            } else {
+                left.add(nameOfNewNode);
+            }
+        }
     }
 
     public List<String> names() {
