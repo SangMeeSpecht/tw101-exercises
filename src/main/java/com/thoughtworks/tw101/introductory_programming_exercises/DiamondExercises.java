bc7b732e72692709006b1e7fd3c6858f23e7fd6b
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-14 13:49:04 -0600

    Add #drawAnIsocelesTriangle and #drawADiamond

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,9 +1,15 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class DiamondExercises {
     public static void main(String[] args) {
+        System.out.println("\nDRAW A ISOSCELES TRIANGLE");
         drawAnIsoscelesTriangle(3);
+        System.out.println("\nDRAW A DIAMOND");
         drawADiamond(8);
+        System.out.println("\nDRAW A DIAMOND WITH YOUR NAME");
         drawADiamondWithYourName(3);
     }
 
@@ -13,7 +19,52 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        renderTopRows(n);
+        renderMiddleRow(n);
+    }
 
+    private static void renderTopRows(int middleRowNum) {
+        for (int currentRowNum = 1; currentRowNum < middleRowNum; currentRowNum++) {
+            System.out.println(renderRow(currentRowNum, middleRowNum));
+        }
+    }
+
+    private static void renderBottomRows(int middleRowNum) {
+        int totalRows = 1 + 2 * (middleRowNum - 1);
+
+        for (int currentRowNum = middleRowNum + 1; currentRowNum <= totalRows; currentRowNum++) {
+            System.out.println(renderRow(middleRowNum - (currentRowNum - middleRowNum), middleRowNum));
+        }
+    }
+
+    private static String addCenteringSpaces(int currentRowNum, int middleRowNum) {
+        String spaces = "";
+        int numOfSpaces = middleRowNum - currentRowNum;
+
+        for (int i = 0; i < numOfSpaces; i++) {
+            spaces += " ";
+        }
+        return spaces;
+    }
+
+    private static String renderRow(int currentRowNum, int middleRowNum) {
+        int stars = 2 * currentRowNum - 1;
+        String row = addCenteringSpaces(currentRowNum, middleRowNum);
+
+        for (int i = 1; i <= stars; i++) {
+            row += "*";
+        }
+        return row;
+    }
+
+    private static void renderMiddleRow(int middleRowNum) {
+        String centerString = "";
+        int numOfStars = middleRowNum * 2 - 1;
+
+        for (int i = 1; i <= numOfStars; i++) {
+            centerString += "*";
+        }
+        System.out.println(centerString);
     }
 
 //    Diamond
@@ -24,7 +75,9 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        renderTopRows(n);
+        renderMiddleRow(n);
+        renderBottomRows(n);
     }
 
 //    Diamond with Name
